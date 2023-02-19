793c3efcd957c954777ac49996cf178df444989c
sheikh salah uddin <skuddin.ius@gmail.com>
2023-02-19 23:20:55 +0600

    basic exercise of c

---
+++ hello.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+    // int a, b, sum;
+    // printf("Enter a : ");
+    // scanf("%d",&a);
+
+    // printf("Enter b : ");
+    // scanf("%d",&b);
+
+    // sum = a + b ; 
+    // printf("sum is  : %d",sum);
+
+    //Area of square
+    // float side;
+    // printf("Enter side ");
+    // scanf("%f",&side);
+    // printf("area is : %f",side*side);
+
+
+    //Area of Circle
+    float  radius, pi = 3.14;
+
+    printf("Enter radius of circle ");
+    scanf("%f",&radius);
+    printf("Area of circle is : %f",pi*radius*radius); 
+    return 0;
+}
